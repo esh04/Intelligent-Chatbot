0cf6bf9f6a3a9a0afe43e18b9a6702c423423489
Esh <eshika.k@research.iiit.ac.in>
2021-11-02 14:33:56 +0530

    statistic function

---
+++ statistics.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+# https://towardsdatascience.com/tracking-corona-covid-19-spread-in-india-using-python-40ef8ffa7e31
+def covidStatistic(state):
+    state = state.title()
+    url = "https://www.oneindia.com/coronavirus-affected-cities-districts-in-india.html"
+    
+    # make a GET request to fetch the raw HTML content
+    web_content = requests.get(url).content
+    
+    # parse the html content
+    soup = BeautifulSoup(web_content, "html.parser")
+    
+    table = soup.find(id=state) 
+    if table is None:
+        print("No such state exists in India. Please recheck your spelling.")
+        return
+
+    rows = table.findAll('td')
+    total = rows[1].text.strip().split('  ')
+    deaths = rows[2].text.strip().split('  ')
+    recovered = rows[3].text.strip().split('  ')
+    print('Covid Statistics of ',state,'\nTotal Cases: ',total[0],'; Total New Cases: ', total[1],'\nTotal Deaths: ',deaths[0],'; Total New Deaths: ', deaths[1],'\nTotal Recovered: ',recovered[0],'; Total New Recovered: ', recovered[1] )
+
